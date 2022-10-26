347c929dbd3c73f01a7314ba03c87c5b5e9173ed
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2022-10-26 12:25:59 +0400

    new commit

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,8 @@
 def palindrome(string):
-    res = "".join(reversed(string))
-    if string == res:
+    s1 = string.replace(' ', '')
+    s1 = s1.lower()
+    s2 = s1[::-1]
+    if s1 == s2:
         return True
     else:
         return False
