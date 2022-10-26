dcaffe675fc2eaee82ee132b4ccd5036ea7633ae
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2022-10-26 11:42:25 +0400

    new commit

---
+++ pangram.py
@@ -0,0 +1,16 @@
+def pangram(string):
+    list = []
+    if len(string) < 26:
+        return len(string)
+    for i in string:
+        for j in i:
+            if j not in list:
+                list.append(j)
+            else:
+                list.remove(j)
+    if len(list) == len(string):
+        return True
+    else:
+        return False
+string = 'qwertyuioplkjhgfdsazxcvbnm'
+print(pangram(string))
